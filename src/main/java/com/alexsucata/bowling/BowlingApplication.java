5c2fbaeb213e085b016b756d19376196466074bc
alexsucata <alex.sucata@yahoo.com>
2021-09-20 06:23:43 +0300

    Implementing CommandLineRunner

--- src/main/java/com/alexsucata/bowling/BowlingApplication.java
+++ src/main/java/com/alexsucata/bowling/BowlingApplication.java
@@ -1,13 +1,29 @@
 package com.alexsucata.bowling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
 
 @SpringBootApplication
-public class BowlingApplication {
+@Profile("bowling")
+public class BowlingApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BowlingApplication.class, args);
-	}
+    private static Logger logger = LoggerFactory.getLogger(BowlingApplication.class);
 
+    public static void main(String[] args) {
+        logger.info("Starting the Bowling application");
+        SpringApplication.run(BowlingApplication.class, args);
+        logger.info("Ending the Bowling application");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Starting collecting data{}", Arrays.toString(args));
+    }
 }
